4bf0373fe1d654b3f144b825a880b116cbded501
Clau03 <90421800+Clau03@users.noreply.github.com>
2022-03-14 00:09:47 +0200

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -20,182 +20,66 @@ int getMax(int arr[], int n)
     return mx;
 }
 
-void bubbleSort(int arr[], int n)
-{
-    int i, j;
-    for (i = 0; i < n - 1; i++)
-
-        // Last i elements are already in place
-        for (j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1])
-                swap(&arr[j], &arr[j + 1]);
-}
 
 void selectionSort(int arr[], int n)
 {
     int i, j, min_idx;
-
-    // One by one move boundary of unsorted subarray
     for (i = 0; i < n - 1; i++)
     {
-        // Find the minimum element in unsorted array
         min_idx = i;
         for (j = i + 1; j < n; j++)
             if (arr[j] < arr[min_idx])
                 min_idx = j;
-
-        // Swap the found minimum element with the first element
         swap(&arr[min_idx], &arr[i]);
     }
 }
 
-void insertionSort(int arr[], int n)
-{
-    int i, key, j;
-    for (i = 1; i < n; i++)
-    {
-        key = arr[i];
-        j = i - 1;
-
-        /* Move elements of arr[0..i-1], that are
-        greater than key, to one position ahead
-        of their current position */
-        while (j >= 0 && arr[j] > key)
-        {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
 
 void heapify(int arr[], int n, int i)
 {
-    int largest = i; // Initialize largest as root
-    int l = 2 * i + 1; // left = 2*i + 1
-    int r = 2 * i + 2; // right = 2*i + 2
-
-    // If left child is larger than root
+    int largest = i; 
+    int l = 2 * i + 1; 
+    int r = 2 * i + 2; 
     if (l < n && arr[l] > arr[largest])
         largest = l;
-
-    // If right child is larger than largest so far
     if (r < n && arr[r] > arr[largest])
         largest = r;
-
-    // If largest is not root
     if (largest != i) {
         swap(arr[i], arr[largest]);
-
-        // Recursively heapify the affected sub-tree
         heapify(arr, n, largest);
     }
 }
-
-// main function to do heap sort
 void heapSort(int arr[], int n)
 {
-    // Build heap (rearrange array)
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
 
-    // One by one extract an element from heap
     for (int i = n - 1; i > 0; i--) {
-        // Move current root to end
         swap(arr[0], arr[i]);
-
-        // call max heapify on the reduced heap
         heapify(arr, i, 0);
     }
 }
 
 void countSort(int arr[], int n, int exp)
 {
-    int* output = new int[1000000]; // output array
+    int* output = new int[1000000]; 
     int i, count[10] = { 0 };
-
-    // Store count of occurrences in count[]
     for (i = 0; i < n; i++)
         count[(arr[i] / exp) % 10]++;
-
-    // Change count[i] so that count[i] now contains actual
-    //  position of this digit in output[]
     for (i = 1; i < 10; i++)
         count[i] += count[i - 1];
-
-    // Build the output array
     for (i = n - 1; i >= 0; i--) {
         output[count[(arr[i] / exp) % 10] - 1] = arr[i];
         count[(arr[i] / exp) % 10]--;
     }
-
-    // Copy the output array to arr[], so that arr[] now
-    // contains sorted numbers according to current digit
     for (i = 0; i < n; i++)
         arr[i] = output[i];
 }
 
-void countingSort(int array[], int size)
-{
-
-    int* output = new int[1000000];
-    int* count = new int[1000000];
-    int max = array[0];
-
-    //Find the largest element of the array
-    for (int i = 1; i < size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
-
-    //Initializing count array with zeros.
-    for (int i = 0; i <= max; ++i)
-    {
-        count[i] = 0;
-    }
-
-    //Storing the count of each element
-    for (int i = 0; i < size; i++) {
-        count[array[i]]++;
-    }
-
-    //Storing the cumulative count of each array
-    for (int i = 1; i <= max; i++) {
-        count[i] += count[i - 1];
-    }
-
-    /*Finding the index of each element of the original array in count array,
-    and place the elements in output array*/
-    for (int i = size - 1; i >= 0; i--) {
-        output[count[array[i]] - 1] = array[i];
-        count[array[i]]--;
-    }
-
-
-    for (int i = 0; i < size; i++) {
-        array[i] = output[i];
-    }
-}
-
-//printing function
-void display(int array[], int size)
-{
-    for (int i = 0; i < size; i++)
-        cout << array[i] << endl;
-    cout << endl;
-}
-
-// The main function to that sorts arr[] of size n using
-// Radix Sort
 void radixsort(int arr[], int n)
 {
-    // Find the maximum number to know number of digits
     int m = getMax(arr, n);
 
-    // Do counting sort for every digit. Note that instead
-    // of passing digit number, exp is passed. exp is 10^i
-    // where i is current digit number
     for (int exp = 1; m / exp > 0; exp *= 10)
         countSort(arr, n, exp);
 }
@@ -203,16 +87,14 @@ void radixsort(int arr[], int n)
 
 int partition(int arr[], int low, int high)
 {
-    int pivot = arr[high];    // pivot
-    int i = (low - 1);  // Index of smaller element
+    int pivot = arr[high];
+    int i = (low - 1);
 
     for (int j = low; j <= high - 1; j++)
     {
-        // If current element is smaller than or
-        // equal to pivot
         if (arr[j] <= pivot)
         {
-            i++;    // increment index of smaller element
+            i++;
             swap(&arr[i], &arr[j]);
         }
     }
@@ -220,20 +102,11 @@ int partition(int arr[], int low, int high)
     return (i + 1);
 }
 
-/* The main function that implements QuickSort
- arr[] --> Array to be sorted,
-  low  --> Starting index,
-  high  --> Ending index */
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
-        /* pi is partitioning index, arr[p] is now
-           at right place */
         int pi = partition(arr, low, high);
-
-        // Separately sort elements before
-        // partition and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
@@ -244,25 +117,18 @@ void merge(int arr[], int l, int m, int r)
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Create temp arrays
     int* L = new int[500000];
     int* R = new int[500000];
 
-    // Copy data to temp arrays L[] and R[]
     for (int i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (int j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
 
-    // Merge the temp arrays back into arr[l..r]
-
-    // Initial index of first subarray
     int i = 0;
 
-    // Initial index of second subarray
     int j = 0;
 
-    // Initial index of merged subarray
     int k = l;
 
     while (i < n1 && j < n2) {
@@ -276,17 +142,11 @@ void merge(int arr[], int l, int m, int r)
         }
         k++;
     }
-
-    // Copy the remaining elements of
-    // L[], if there are any
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
-
-    // Copy the remaining elements of
-    // R[], if there are any
     while (j < n2) {
         arr[k] = R[j];
         j++;
@@ -296,25 +156,14 @@ void merge(int arr[], int l, int m, int r)
     delete R;
 }
 
-// l is for left index and r is
-// right index of the sub-array
-// of arr to be sorted */
 void mergeSort(int arr[], int l, int r) {
     if (l >= r) {
-        return;//returns recursively
+        return;
     }
     int m = l + (r - l) / 2;
     mergeSort(arr, l, m);
     mergeSort(arr, m + 1, r);
     merge(arr, l, m, r);
-}
-
-// UTILITY FUNCTIONS
-// Function to print an array
-void printArray(int A[], int size)
-{
-    for (int i = 0; i < size; i++)
-        cout << A[i] << " ";
 }
 
 static void bucketSort(int* data, int count) {
@@ -369,19 +218,14 @@ int main()
     }
     clkStart = clock();
     //Uncomment the algorithm you want to use
-
-
 //    selectionSort(arr, size);
-    //insertionSort(arr, size);
 //    mergeSort(arr, 0, size - 1);
 //    quickSort(arr, 0, size - 1);
 //    heapSort(arr, size);
 //    radixsort(arr, size);
-//    countingSort(arr, size);
 //    bucketSort(arr,size);
 
     clkFinish = clock();
-//    printArray(arr, size);
 
     cout << endl << clkFinish - clkStart << " ms" << endl;
     input.close();
