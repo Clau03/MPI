1c3515d66c0838697edaed1faed58819df51ef99
Clau03 <90421800+Clau03@users.noreply.github.com>
2022-03-14 00:13:38 +0200

    Create generator.cpp

---
+++ generator.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+ofstream fout("D:\\C++\\dataclo.txt");
+//Uncomment the main function you want to use
+
+//int main()
+//{
+//    int x;
+//    for(long i=1;i<=10000;i++)
+//    {
+//        x = rand()%11;
+//        fout<<x<<" ";
+//    }
+//    fout.close();
+//    return 0;
+//}
+
+//int main()
+//{
+//    for(int i=10000;i>=0;i--)
+//    {
+//        fout<<i<<" ";
+//    }
+//    fout.close();
+//    return 0;
+//}
+
+//int main()
+//{
+//    int x;
+//    for(long i=1;i<=100000;i++)
+//    {
+//        x = rand();
+//        fout<<x<<" ";
+//    }
+//    fout.close();
+//    return 0;
+//}
+
+//int main()
+//{
+//    for(int i=0;i<=10000;i++)
+//    {
+//        fout<<i<<" ";
+//    }
+//    fout.close();
+//    return 0;
+//}
